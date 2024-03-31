f3cc65631c87050cb06870b9cb21095a48b72d38
ucharles28 <uzomacharles28@gmail.com>
2024-03-31 14:52:12 +0100

    Add rate limiter

--- YARP.Sample/Program.cs
+++ YARP.Sample/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Rate limiter
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
